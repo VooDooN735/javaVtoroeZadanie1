d848ee97c3d5b1f899ab308eae20ce50ac8cdef0
VooDooN735 <107447207+VooDooN735@users.noreply.github.com>
2022-07-07 21:43:17 +0300

    MILI

---
+++ src/MILI.java
@@ -0,0 +1,13 @@
+public class MILI {
+    public static void main(String[] args) {
+            int airTicketPrice = 5750;
+            int oneMil = 20;
+            int bonusMili = airTicketPrice/oneMil;
+            System.out.println(bonusMili + " бонусных мили");
+
+
+
+
+    }
+
+}
